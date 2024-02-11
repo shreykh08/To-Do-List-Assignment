5ca503d57753e3cab102c0b6d5a778e5c5449be1
Shrey Khandelwal <125605145+shreykh08@users.noreply.github.com>
2024-02-12 01:35:37 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import TodoListContainer from "./components/TodoListContainer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <TodoListContainer />
+    </>
+  );
+}
+
+export default App;
